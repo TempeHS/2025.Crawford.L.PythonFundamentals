63785a2a40acdd6ae69bd7c0715aaa235842c37c
Crawford Lieu <crawford.lieu@education.nsw.gov.au>
2024-02-06 08:11:10 +0000

    Add calculator

--- 0-FunctionsVariables/hello.py
+++ 0-FunctionsVariables/hello.py
@@ -1,2 +1,4 @@
-name = input("What's your name? ")
-print("Hello, " + name + "!")
+name = input("What's your name? ")  # Ask the user for their name
+name = name.strip().title()  # Clean up the name
+# first, last = name.split(" ")  # Split the name into first and last
+print(f'Hello, "{name}"!')  # Print the user's name
---
+++ calculator.py
@@ -0,0 +1,4 @@
+x = int(input("Enter x: "))  # Ask the user for x
+y = int(input("Enter y: "))  # Ask the user for y
+
+print(x + y)
