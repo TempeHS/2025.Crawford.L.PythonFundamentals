18d84b70683799e0ef9d67399e4d9a8963c162b1
Crawford Lieu <crawford.lieu@education.nsw.gov.au>
2024-02-06 21:31:05 +1100

    test push 2
    Signed-off-by: Crawford Lieu <crawford.lieu@education.nsw.gov.au>

--- 0-FunctionsVariables/calculator.py
+++ 0-FunctionsVariables/calculator.py
@@ -2,4 +2,6 @@ x = int(input("Enter x: "))  # Ask the user for x
 y = int(input("Enter y: "))  # Ask the user for y
 z = int(input("Enter z: "))  # Ask the user for z
 
-print(x + y + z)
+
+
+print(x + y + z) # Print the sum of x, y, and z
