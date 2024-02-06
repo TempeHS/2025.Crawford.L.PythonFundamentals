21b85dd01efcc4e7ceacd13968479f9b79ed5f7d
Crawford Lieu <110504159+DefNotCrawf@users.noreply.github.com>
2024-02-06 21:45:50 +1100

    test push 6
    Signed-off-by: Crawford Lieu <110504159+DefNotCrawf@users.noreply.github.com>

--- 0-FunctionsVariables/calculator.py
+++ 0-FunctionsVariables/calculator.py
@@ -5,3 +5,4 @@ z = int(input("Enter z: "))  # Ask the user for z
 print(x + y + z) # Print the sum of x, y, and z
 print(x - y - z) # Print the difference of x, y, and z
 print(x * y * z) # Print the product of x, y, and z
+print(x / y / z) # Print the quotient of x, y, and z
