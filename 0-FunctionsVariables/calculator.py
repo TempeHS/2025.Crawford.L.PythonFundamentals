bbc2592331490e53270533b8b42de4419c3378e2
Crawford Lieu <110504159+DefNotCrawf@users.noreply.github.com>
2024-02-08 09:39:48 +1100

    Update calculator.py

--- 0-FunctionsVariables/calculator.py
+++ 0-FunctionsVariables/calculator.py
@@ -1,8 +1,5 @@
 x = float(input("Enter x: "))  # Ask the user for x
 y = float(input("Enter y: "))  # Ask the user for y
-z = float(input("Enter z: "))  # Ask the user for z
+z = round(x / y, 2)
 
-print(x + y + z) # Print the sum of x, y, and z
-print(x - y - z) # Print the difference of x, y, and z
-print(x * y * z) # Print the product of x, y, and z
-print(x / y / z) # Print the quotient of x, y, and z
+print(z)
