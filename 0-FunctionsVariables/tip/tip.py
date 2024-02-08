c016f24d2111e840ce17f46fa2eab6d5011566a1
Crawford Lieu <110504159+DefNotCrawf@users.noreply.github.com>
2024-02-08 20:11:16 +1100

    Upload completed tip.py (again)

--- 0-FunctionsVariables/tip/tip.py
+++ 0-FunctionsVariables/tip/tip.py
@@ -7,11 +7,13 @@ def main():
 	print(f"Leave ${tip:.2f}")
 
 def dollars_to_float(d):
+	d = d.replace("$","")
 	d = float(d)
 	d = round(d, 2)
 	return d
 
 def percent_to_float(p):
+   p = p.replace("%","")
    p = float(p)
    p = p / 100
    return p
