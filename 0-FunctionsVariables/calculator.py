616f54416e8d8581fb3b2a1d734b991ceb185915
Crawford Lieu <110504159+DefNotCrawf@users.noreply.github.com>
2024-02-09 13:47:24 +1100

    Upload function.py and import hello.py into it

--- 0-FunctionsVariables/calculator.py
+++ 0-FunctionsVariables/calculator.py
@@ -1,5 +1,6 @@
 x = float(input("Enter x: "))  # Ask the user for x
 y = float(input("Enter y: "))  # Ask the user for y
-z = round(x / y, 2)
 
-print(z) # Print the result
+z = x / y
+
+print(f"{z:.2f}") # Print the result
