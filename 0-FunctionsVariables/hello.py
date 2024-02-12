78085174296e03185f74f513e50855f0757a2b05
Crawford Lieu <110504159+DefNotCrawf@users.noreply.github.com>
2024-02-12 11:18:36 +1100

    Mandatory push

--- 0-FunctionsVariables/hello.py
+++ 0-FunctionsVariables/hello.py
@@ -1,4 +1,10 @@
+def hello(to):
+	to = to.strip().title()
+	print(f"Hello, {name}!")
+
+
 name = input("What's your name? ")  # Ask the user for their name
-name = name.strip().title()  # Clean up the name
+
+  # Clean up the name
 # first, last = name.split(" ")  # Split the name into first and last
 print(f'Hello, "{name}"!')  # Print the user's name
