6770307dc8c6cdf76dfe83f25cb941a81b314d8c
Crawford Lieu <110504159+DefNotCrawf@users.noreply.github.com>
2024-02-27 20:20:56 +1100

    finish cat.py

--- 2-Loops/cat.py
+++ 2-Loops/cat.py
@@ -1,5 +1,28 @@
-for _ in range(10):
-    print("Meow")
+def main():
+    number = get_number
+    meow(number)
+    
+def get_number():
+    while True:
+        n = int(input("Number of 'Meows'? "))
+        if n > 0:
+            return n
+    
+def meow(n):
+    for _ in range(n):
+        print("Meow")
+
+main()
+
+# while True:
+#     n = int(input("Number of 'Meows'? "))
+#     if n > 0:
+#         break
+
+# print("Meow\n" * n, end="")
+
+# for _ in range(3):
+#     print("Meow")
 
 # i = 0 
 # rep = int(input("Enter a number: ")) # 3
