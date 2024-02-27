08c67346e73f5180cea4d7bb8b2ad7e7fe6270e6
Crawford Lieu <110504159+DefNotCrawf@users.noreply.github.com>
2024-02-27 20:32:59 +1100

    finish hogwarts.py

---
+++ 2-Loops/hogwarts.py
@@ -0,0 +1,11 @@
+students = ["Hermione", "Harry", "Ron"]
+
+for i in range(len(students)):
+    print(i+1, students[i])
+
+# for student in students:
+#     print(student)
+
+# print(students[0])
+# print(students[1])
+# print(students[2])
