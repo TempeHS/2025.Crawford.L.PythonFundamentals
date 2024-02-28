5920e5d82a94cc91700aba79f16cc1ee7bc330f6
Crawford Lieu <110504159+DefNotCrawf@users.noreply.github.com>
2024-02-28 19:16:34 +1100

    finish hogwarts.py

--- 2-Loops/hogwarts.py
+++ 2-Loops/hogwarts.py
@@ -1,7 +1,33 @@
-students = ["Hermione", "Harry", "Ron"]
+students = [
+	{"name": "Hermione", "house": "Gyffindor", "patronus": "Otter"},
+	{"name": "Harry", "house": "Gyffindor", "patronus": "Stag"},
+	{"name": "Ron", "house": "Gyffindor", "patronus": "Jack Russell Terrier"},
+	{"name": "Draco", "house": "Slytherin", "patronus": None}
+]
 
-for i in range(len(students)):
-    print(i+1, students[i])
+for student in students:
+	print(student["name"], student["house"], student["patronus"], sep=": ")
+
+# students = {
+# 	"Hermione": "Gyffindor", 
+# 	"Harry": "Gyffindor", 
+# 	"Ron": "Gyffindor", 
+# 	"Draco": "Slytherin"
+# }
+
+# for student in students:
+# 	print(student, students[student], sep=": ")
+
+# print(students["Hermione"])
+# print(students["Harry"])
+# print(students["Ron"])
+# print(students["Draco"])
+
+# students = ["Hermione", "Harry", "Ron", "Draco"]
+# houses = ["Gyffindor", "Gyffindor", "Gyffindor", "Slytherin"]
+
+# for i in range(len(students)):
+# 	print(i+1, students[i])
 
 # for student in students:
 #     print(student)
