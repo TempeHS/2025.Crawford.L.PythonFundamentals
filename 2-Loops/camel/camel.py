8cc493de94f226bc82a4a83b4e8317a033996b18
Crawford Lieu <110504159+DefNotCrawf@users.noreply.github.com>
2024-03-02 12:35:41 +1100

    Upload completed camel.py

---
+++ 2-Loops/camel/camel.py
@@ -0,0 +1,49 @@
+name = input("Insert a string: ")
+
+# print(name[1:]) # prints all from the second character to the end of the string
+# print(len(name)) # print the length of the string
+# name = name[0].lower() + name[1:] # make the first character of the string lower case
+
+# print(name[:2]) # prints all to the third character of the string excluding the third character
+
+# for c in range(len(name)):
+c = 0
+while True:
+	if name[c].isupper():
+		name = name[:c] + '_' + name[c].lower() + name[c+1:]
+		c += 1
+	elif name[c].islower():
+		c += 1
+		'''
+  		NtS: If the elif statement is not included, the loop will run indefinitely. 
+    	(I thought it would stop when the condition is not met, but it doesn't.)
+    	'''
+	else:
+		c += 1
+		continue
+	if c < len(name):
+		continue
+	else: 
+		break
+
+# Just a complicated do while loop.
+		
+
+print(name)
+
+'''
+Below is another way to solve the problem.
+It takes each character of the string, ckecks it and then appends no matter what it to a new string. It may not be the most efficient way to solve the problem.
+The above solution checks each character of the string and then replaces it with the camel case version. It is more efficient than the below solution. 
+'''
+# 
+# result = []
+# def camel_to_snake(name):
+# 	result = [name[0].lower()]
+# 	for c in name[1:]:
+# 		if c in ('ABCDEFGHIJKLMNOPQRSTUVWXYZ'): # or c.isupper()
+# 			result.append('_')
+# 			result.append(c.lower())
+# 		else:
+# 			result.append(c)
+# 	return ''.join(result)
---
+++ 2-Loops/mario.py
@@ -0,0 +1,33 @@
+def main():
+	print_square(3)
+
+def print_square(n):
+	for _ in range(n):
+		print_row(n)
+
+def print_row(size):
+	for _ in range(size):
+		print("#", end="")
+
+
+
+# 	print_row(n)
+
+# def print_row(width):
+# 	for _ in range(width):
+# 		print("?", end="")
+
+# 	print_colomn(n)
+
+# def print_colomn(height):
+# 	for _ in range(height):
+# 		print("#")
+
+main(
+	# int(
+    # input("Width of block: ")
+    # )
+)
+
+# for _ in range(3):
+# 	print("#")
