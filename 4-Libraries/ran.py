ee951160aff25c1989899036e6591a118b742880
Crawford Lieu <110504159+DefNotCrawf@users.noreply.github.com>
2024-03-11 12:49:25 +1100

    intergrate first and last name

--- 4-Libraries/ran.py
+++ 4-Libraries/ran.py
@@ -1,13 +1,18 @@
 import sys
-if len(sys.argv) > 2:
+print (len(sys.argv))
+if len(sys.argv) > 3:
     print("Too many arguments.")
     sys.exit(1)
-elif len(sys.argv) < 2:
+elif len(sys.argv)- 1 == 0:
     print("You have not entered your name as an argument.")
     sys.exit(1)
-else: 
+elif len(sys.argv)- 1 == 2:
+    print(f"Hello, your name is {sys.argv[1]} {sys.argv[2]}.", end=" ")
+    print(f"You have ran the file '{sys.argv[0]}'.")
+else:
     print(f"Hello, your name is {sys.argv[1]}!", end=" ")
     print(f"You have ran the file '{sys.argv[0]}'.")
+
 # except IndexError:
 #     print("You have not entered your name as an argument.")
     
