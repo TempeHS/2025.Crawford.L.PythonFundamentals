d2ca7206103993c55d60e93c4d6823fed85cf603
Crawford Lieu <110504159+DefNotCrawf@users.noreply.github.com>
2024-03-11 12:25:05 +1100

    card generator

--- 4-Libraries/ran.py
+++ 4-Libraries/ran.py
@@ -1,4 +1,12 @@
 import random
 
-yes = random.choice(["heads", "tails"])
-print(yes)
+cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+random.shuffle(cards)
+random.shuffle(suits)
+
+def draw_card():
+    return cards.pop() + ' of ' + suits.pop()
+
+print (draw_card())
+
