76f5eba08cc71d169821350e8e9ad59664a1b07b
Crawford Lieu <110504159+DefNotCrawf@users.noreply.github.com>
2024-03-20 09:10:20 +1100

    yes

--- class_work/rps_logic.py
+++ class_work/rps_logic.py
@@ -59,34 +59,3 @@ while no_games < request_games:
 				no_games += 1
 		case _:
 			print("Invalid choice. Please type a valid choice.")
-
-'''
-Psudocode:
-
-BEGIN rps_logic
-	INPUT playerChoice = "Rock, Paper or Scissors"
-	FUNCTION generate_random
-	MATCH player_choice
-		CASE rock
-			IF player_choice == paper
-				PRINT "Win"
-			ELSEIF player_choice == scissors
-				PRINT "Lose"
-			ELSE
-				PRINT "Draw"
-		CASE paper
-			IF player_choice == scissors
-				PRINT "Win"
-			ELSEIF player_choice == rock
-				PRINT "Lose"
-			ELSE
-				PRINT "Draw"
-		CASE scissors
-			IF player_choice == rock
-				PRINT "Win"
-			ELSEIF player_choice == paper
-				PRINT "Lose"
-			ELSE
-				PRINT "Draw"
-END rps_logic
-'''
