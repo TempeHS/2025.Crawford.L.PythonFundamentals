b10a724ee2ba7dc6b206637ce9ab0c5a6d1e302f
Crawford Lieu <110504159+DefNotCrawf@users.noreply.github.com>
2024-05-09 08:57:02 +1000

    Update

--- 8-OOP/tester.py
+++ 8-OOP/tester.py
@@ -1,15 +1,15 @@
-def main():
-    student = get_student()
-    if student["name"] == "Padma":
-        student["house"] = "Ravenclaw"
-    print(f"{student['name']} from {student['house']}")
+class Student:
+	...
 
+def main():
+	student = get_student()
+	print(f"{student.name} from {student.house}")
 
 def get_student():
-    name = input("Name: ")
-    house = input("House: ")
-    return {"name": name, "house": house}
-
-
+	student = Student()
+	student.name = input("Name: ")
+	student.house = input("House: ")
+	return student
+	
 if __name__ == "__main__":
-    main()
+	main()
