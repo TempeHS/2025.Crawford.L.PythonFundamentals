1bd618e3e2b2ca435280a7c7cf6cb5c4e102b6a9
Crawford Lieu <110504159+DefNotCrawf@users.noreply.github.com>
2024-05-09 09:32:23 +1000

    up OOP

--- 8-OOP/tester.py
+++ 8-OOP/tester.py
@@ -1,16 +1,25 @@
 class Student:
 	def __init__(self, name, house):
+		# self.name = name
+		# self.house = house
+		if not name:
+			raise ValueError("Name is required.")
+		if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+			raise ValueError("House is not valid.")
 		self.name = name
 		self.house = house
-	def who_from(self):
-		return f"{self.name} from {self.house}"
+	def __str__(self):
+		return f"{self.name} is in {self.house}."
+
 	
+def get_student():
+	name = input("Name: ")
+	house = input("House: ")
+	return Student(name, house)
 
-name = input("Name: ")
-house = input("House: ")
-student = Student(name, house)
 
-
-print(
-	student.who_from()
-)	
+def main():
+	student = get_student()
+	print(student)
+ 
+main()
