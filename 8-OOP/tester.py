8107263af121897d4329e47712f17fa7cee352bb
Crawford Lieu <110504159+DefNotCrawf@users.noreply.github.com>
2024-05-10 14:14:29 +1000

    up OOP 3

--- 8-OOP/tester.py
+++ 8-OOP/tester.py
@@ -1,15 +1,28 @@
+from re import S
+
+
 class Student:
-	def __init__(self, name, house, patronus):
+	def __init__(self, name, house):
 		# self.name = name
 		# self.house = house
 		# if not name:
 		# 	raise ValueError("Name is required.")
 		self.name = name
 		self.house = house
-		self.patronus = patronus
+
 
 	def __str__(self):
 		return f"{self.name} is in {self.house}."
+
+	@property
+	def name(self):
+		return self._name
+
+	@name.setter
+	def name(self, name):
+		if not name:
+			raise ValueError("Name is required.")
+		self._name = name
 
 	@property # getter
 	def house(self):
@@ -25,16 +38,13 @@ class Student:
 def get_student():
 	name = input("Name: ")
 	house = input("House: ")
-	patronus = input("Patronus: ")
-	return Student(name, house, patronus)
+	return Student(name, house,)
 
 
 def main():
 	student = get_student()	# This function will return a Student class object
 							# NtS: make sure a variable is eventually assigned to the class object
+	student._house = "Number Four, Privet Drive"
 	print(student)
 
 main()
-
-print("End of tester.py")
-
